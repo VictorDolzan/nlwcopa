8a11c9a049347b1d3845634e0a20cd4f3d6c9a34
Victor Dolsan <victor.freitas@mazzafc.tech>
2022-11-06 11:30:04 -0300

    Subindo components

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from "./src/components/Loading";
 import { THEME } from './src/styles/theme';
-import { SignIng } from "./src/screens/SignIng";
+import { New } from "./src/screens/New";
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
                 backgroundColor="transparent"
                 translucent
             />
-                {fontsLoaded ? <SignIng /> : <Loading />}
+                {fontsLoaded ? <New /> : <Loading />}
             </AuthContextProvider>
         </NativeBaseProvider>
     );
