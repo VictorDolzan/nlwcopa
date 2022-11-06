eeab1c4142df250fae3c7182c42a0c52e3dd5122
Victor Dolsan <victor.freitas@mazzafc.tech>
2022-11-05 21:31:05 -0300

    Feita a tela de SignIn Criado o AuthContext para autenticação de usuário

--- App.tsx
+++ App.tsx
@@ -3,18 +3,21 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { Loading } from "./src/components/Loading";
 import { THEME } from './src/styles/theme';
 import { SignIng } from "./src/screens/SignIng";
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
     const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
     return (
         <NativeBaseProvider theme={THEME}>
+            <AuthContextProvider>
             <StatusBar
                 barStyle="light-content"
                 backgroundColor="transparent"
                 translucent
             />
                 {fontsLoaded ? <SignIng /> : <Loading />}
+            </AuthContextProvider>
         </NativeBaseProvider>
     );
 }
